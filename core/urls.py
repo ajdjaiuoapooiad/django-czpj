6c060b48b43350bd7d2b27673605dffe95211de2
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-24 14:31:18 +0900

    11ch ok

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,11 @@ from django.urls import include, path
 from core import views
 
 urlpatterns = [
-   path('',views.home),
+   path('',views.home,name='home'),
+   path('about',views.about,name='about'),
+   path('service',views.service,name='service'),
+   path('contact',views.contact,name='contact'),
+
+
+
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def about(request):
+    return render(request,'about.html')
+
+
+def service(request):
+    return render(request,'service.html')
+
+
+def contact(request):
+    return render(request,'contact.html')
