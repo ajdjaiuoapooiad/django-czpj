2ff9beec0b2cd16f48f00b448700e66a5a6d9b5a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-26 15:54:57 +0900

    31ch ok

--- core/views.py
+++ core/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
+from cars.models import Car
 from core.models import Team
 
 
 def home(request):
     teams = Team.objects.all()
+    featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
+
+    
+    
+    
     data = {
             'teams': teams,
+            'featured_cars': featured_cars,
+
             
         }
     return render(request, 'home.html', data)
