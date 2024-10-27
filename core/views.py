1fde7b3a95e25f8feb8684e1feb6d4be9efc84a9
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-27 14:14:09 +0900

    search ok

--- core/views.py
+++ core/views.py
@@ -9,13 +9,22 @@ def home(request):
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_date')
     
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
     
     
     data = {
             'teams': teams,
             'featured_cars': featured_cars,
             'all_cars': all_cars,
-
+    
+            'model_search': model_search,
+            'city_search': city_search,
+            'year_search': year_search,
+            'body_style_search': body_style_search,
             
         }
     return render(request, 'home.html', data)
@@ -23,6 +32,7 @@ def home(request):
 
 def about(request):
     teams = Team.objects.all()
+    
     data = {
             'teams': teams,
             
