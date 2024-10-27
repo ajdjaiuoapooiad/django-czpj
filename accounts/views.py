52ebf6552e3b8dce04fab0acf8a6594f55b8cebb
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-27 17:19:28 +0900

    login 54ch ok

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.contrib import messages, auth
+
+from django.shortcuts import redirect, render
 
 
 def login(request):
-    return render(request,'accounts/login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+
+
 
 def register(request):
     return render(request,'accounts/register.html')
